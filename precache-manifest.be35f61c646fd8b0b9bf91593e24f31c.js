79bc2b036ea8d9171d976ee3cf90db04d543f672
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-29 03:14:21 +0000

    Deploy manh-vv/react-snake-game to github.com/manh-vv/react-snake-game.git:gh-pages

---
+++ precache-manifest.be35f61c646fd8b0b9bf91593e24f31c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0e7fdf3b87756543f2689d744312f94",
+    "url": "/react-snake-game/index.html"
+  },
+  {
+    "revision": "e2f767ed553604fb963f",
+    "url": "/react-snake-game/static/css/main.88a76987.chunk.css"
+  },
+  {
+    "revision": "a8548ca9bb7388365643",
+    "url": "/react-snake-game/static/js/2.a0721c7e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-snake-game/static/js/2.a0721c7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2f767ed553604fb963f",
+    "url": "/react-snake-game/static/js/main.e6202f48.chunk.js"
+  },
+  {
+    "revision": "29e856bcd72f0474f31a",
+    "url": "/react-snake-game/static/js/runtime-main.0f2d89e1.js"
+  }
+]);
